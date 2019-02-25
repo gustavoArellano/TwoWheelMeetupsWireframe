62283ae3c2aa47c192a674832b2bae3ea96a2457
Gustavo Arellano <gustavo813arellano@gmail.com>
2019-02-24 16:21:11 -0800

    created main pages with headers pre configured

---
+++ js/profile.js
@@ -0,0 +1,17 @@
+//DROP DOWN MENU FOR TABLET MODE
+function myFunction() {
+    document.getElementById("tabletDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.menuButton')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
